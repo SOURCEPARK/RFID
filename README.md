21d2d9b9a58599e52e36c076b8a036fbed0436c0
root <root@kali.de>
2015-10-20 19:10:35 +0200

    README.md hinzugefügt

---
+++ README.md
@@ -0,0 +1,16 @@
+#Download des Linux Treibers
+Als erstes muss der Treiber herrunter geladen werden.
+
+Link:
+
+http://www.acs.com.hk/download-driver-unified/6258/ACS-Unified-Driver-Lnx-Mac-110-P.zip
+
+#Installation des Treibers
+
+1.als root anmelden
+
+2. Entpacken des Treibers
+
+ **tar -jxvf acsccid-x.y.z.tar.bz2**
+ 
+ **cd acsccid-x.y.z**
--- src/rfid/Rfid.java
+++ src/rfid/Rfid.java
@@ -1,26 +1,16 @@
-/*
-Copyright (c) 2011, Gerhard H. Schalk (www.smartcard-magic.net)
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the 
-documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
-USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 package rfid;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import javax.smartcardio.*;
 
 public class Rfid {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        //driver: http://www.acs.com.hk/download-driver-unified/6258/ACS-Unified-Driver-Lnx-Mac-110-P.zip
         try{
 
             // Liste mit allen verfügbaren Terminals
@@ -62,6 +52,11 @@ public class Rfid {
                     System.out.printf("%02X ", baCardUid [i]);
                 }
             }
+            
+            
+            URL url = new URL("http://localhost:8888/auth/123456");
+     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+     urlConnection.disconnect();
 
         card.disconnect(false);
 
